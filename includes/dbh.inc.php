27789c80a8f7134195f602fc15b83e4c39395503
LouisRadek <radek.louis@gmail.com>
2022-06-06 21:43:56 +0200

    changes

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -4,10 +4,11 @@ $serverName = "localhost";
 $dBUserName = "root";
 $dBPassword = "";
 $dBName = "websitedb";
-error_reporting(0);
+
 $conn = mysqli_connect($serverName, $dBUserName, $dBPassword, $dBName);
-error_reporting(E_ALL);
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+
+if ($conn->connect_error) {
+   die("Connection failed: " . mysqli_connect_error() );
 }
+
 
